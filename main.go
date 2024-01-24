73465a2fc030c3c4cef910b2240a40747ca6836b
Saufi <msaufi2325@gmail.com>
2024-01-25 02:20:36 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"channels/helpers"
+)
+
+const numPool = 10
+
+func CalculateValue(intChan chan int) {
+	randomNumber := helpers.RandomNumber(numPool)
+	intChan <- randomNumber
+}
+
+func main() {
+	intChan := make(chan int)
+	defer close(intChan)
+}
